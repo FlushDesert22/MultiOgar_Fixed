3b3e7971805f1e5b7dfe518830dfe66cb576219b
FlushDesert22 <41596690+FlushDesert22@users.noreply.github.com>
2023-12-22 23:13:43 -0500

    Update AddNode.js
    Fixed another dumb mistake.

--- src/packet/AddNode.js
+++ src/packet/AddNode.js
@@ -11,6 +11,6 @@ module.exports = AddNode;
 AddNode.prototype.build = function (protocol) {
     var writer = new BinaryWriter();
     writer.writeUInt8(0x20); // Packet ID
-    writer.writeUInt32((this.item.nodeId ^ this.player.scrambleId) >>> 0);
+    writer.writeUInt32((this.item.nodeId ^ this.playerTracker.scrambleId) >>> 0);
     return writer.toBuffer();
 };
